7caab8d59cab5e3881f5f9e4d6e0646377f341ea
Peter Folta <mail@peterfolta.net>
2017-12-28 21:44:27 +0000

    Add UiHandler class

---
+++ js/ui_handler.js
@@ -0,0 +1,23 @@
+class UiHandler {
+  constructor(game) {
+    this.game = game;
+
+    this.uiControls = {
+      SCORE_LABEL: document.getElementById("js-score"),
+      HIGHSCORE_LABEL: document.getElementById("js-highscore")
+    }
+  }
+
+  updateUiControls() {
+    this.updateScore(this.game.score);
+    this.updateHighscore(this.game.highscore);
+  }
+
+  updateScore(score) {
+    this.uiControls.SCORE_LABEL.innerHTML = score;
+  }
+
+  updateHighscore(highscore) {
+    this.uiControls.HIGHSCORE_LABEL.innerHTML = highscore;
+  }
+}
