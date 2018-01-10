5bb9f00cefd856ddd184d19716f713d45810a4b5
Peter Folta <mail@peterfolta.net>
2018-01-10 22:55:49 +0000

    Add a first basic sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 const gulp = require("gulp");
+const sass = require("gulp-ruby-sass");
+
+gulp.task("sass", () => {
+  return sass(
+      "app/styles/application.scss",
+      {
+        emitCompileError: true,
+        stopOnError: true
+      })
+    .pipe(gulp.dest("dist/styles"));
+});
 
 gulp.task("default", () => console.log("default task"));
