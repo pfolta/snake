89f73e466ad45d59f93260f804c605273e6c89b7
Peter Folta <mail@peterfolta.net>
2018-01-10 23:18:12 +0000

    Add dependency for gulp-autoprefixer and add simple autoprefix-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const del = require("del");
 const sass = require("gulp-ruby-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("clean", () => {
   return del.sync("dist");
@@ -13,6 +14,14 @@ gulp.task("sass", () => {
         emitCompileError: true,
         stopOnError: true
       })
+    .pipe(gulp.dest("dist/styles"));
+});
+
+gulp.task("autoprefix-css", () => {
+  return gulp.src("dist/styles/*.css")
+    .pipe(autoprefixer({
+      browsers: "> 1%, last 2 versions, Firefox ESR"
+    }))
     .pipe(gulp.dest("dist/styles"));
 });
 
