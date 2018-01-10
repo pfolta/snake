88658b723c331ff730fdb2a71c443a1b6e73a055
Peter Folta <mail@peterfolta.net>
2018-01-10 23:41:25 +0000

    Add dependencies for gulp-cssnano and gulp-rename and add basic minify-css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require("gulp");
 const del = require("del");
+const rename = require("gulp-rename");
 const sass = require("gulp-ruby-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
 
 gulp.task("clean", () => {
   return del.sync("dist");
@@ -20,8 +22,19 @@ gulp.task("sass", () => {
 gulp.task("autoprefix-css", () => {
   return gulp.src("dist/styles/*.css")
     .pipe(autoprefixer({
-      browsers: "> 1%, last 2 versions, Firefox ESR"
-    }))
+        browsers: "> 1%, last 2 versions, Firefox ESR"
+      })
+    )
+    .pipe(gulp.dest("dist/styles"));
+});
+
+gulp.task("minify-css", () => {
+  return gulp.src(["dist/styles/*.css", "!dist/styles/*.min.css"])
+    .pipe(rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(cssnano())
     .pipe(gulp.dest("dist/styles"));
 });
 
