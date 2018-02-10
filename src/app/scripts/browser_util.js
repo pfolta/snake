800d2a48a820318fe420b8e2bde5aacb4618b93c
Peter Folta <mail@peterfolta.net>
2018-02-10 16:09:53 +0000

    Remove unnecessary BrowserUtil class

--- src/app/scripts/browser_util.js
+++
@@ -1,9 +0,0 @@
-export default class BrowserUtil {
-  static isAppleIOs() {
-    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-  }
-
-  static isAppleIOsStandalone() {
-    return BrowserUtil.isAppleIOs() && window.navigator.standalone;
-  }
-}
