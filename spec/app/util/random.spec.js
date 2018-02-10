b77335cd4b4a527c3ef9d83f047bdbf3b1844d3f
Peter Folta <mail@peterfolta.net>
2018-02-10 17:42:49 +0000

    Refactor unit tests for Random class

--- spec/app/util/random.spec.js
+++ spec/app/util/random.spec.js
@@ -4,39 +4,51 @@ import Random from '../../../src/app/scripts/util/random';
 
 describe('Random', () => {
   describe('random()', () => {
+    let result;
+
+    beforeEach(() => {
+      result = Random.random();
+    });
+
     it('should return a number', () => {
-      let result = Random.random();
       assert.isNumber(result);
     });
 
     it('should return a number 0 <= n < 1', () => {
-      let result = Random.random();
       assert.isAtLeast(result, 0);
       assert.isBelow(result, 1);
     });
   });
 
   describe('random(5)', () => {
+    let result;
+
+    beforeEach(() => {
+      result = Random.random(5);
+    });
+
     it('should return a number', () => {
-      let result = Random.random(5);
       assert.isNumber(result);
     });
 
     it('should return a number 0 <= n < 5', () => {
-      let result = Random.random(5);
       assert.isAtLeast(result, 0);
       assert.isBelow(result, 5);
     });
   });
 
   describe('random(1, 10)', () => {
+    let result;
+
+    beforeEach(() => {
+      result = Random.random(1, 10);
+    });
+
     it('should return a number', () => {
-      let result = Random.random(1, 10);
       assert.isNumber(result);
     });
 
     it('should return a number 1 <= n < 10', () => {
-      let result = Random.random(1, 10);
       assert.isAtLeast(result, 1);
       assert.isBelow(result, 10);
     });
